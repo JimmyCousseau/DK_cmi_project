0f0238e256417b20bff7fb5c9dae3cf8a51a7aa7
JimmyCousseau <101827067+JimmyCousseau@users.noreply.github.com>
2022-04-14 13:02:17 +0200

    Update Text.cc

--- Text.cc
+++ Text.cc
@@ -1,13 +1,11 @@
 #include "Text.hpp"
 
 Text::Text(std::string text, float x, float y, int size, sf::Font font, sf::Color color)
-    : Element(x, y, color), m_text(text), m_font(font), m_size
+    : Element(x, y), m_text(text), m_font(font), m_size(size)
 {
 }
 
-Text::~Text()
-{
-}
+Text::~Text() {}
 
 // Draw text into the window
 void Text::draw(sf::RenderWindow &window)
@@ -18,7 +16,6 @@ void Text::draw(sf::RenderWindow &window)
     text.setCharacterSize(m_size);
     text.setFillColor(m_color);
     text.setPosition(m_x, m_y);
-    game.set_text_score(m_text);
     window.draw(text);
 }
 
@@ -27,5 +24,5 @@ void Text::set_text() {}
 
 
 // Getters
-Vector2D Rectangle::get_center() const {}
-Vector2D Rectangle::get_futhest_point(const Vector2D &direction) const {}
+Vector2D Text::get_center() const {}
+Vector2D Text::get_futhest_point(const Vector2D &direction) const {}
